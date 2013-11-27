53582aadef0078536256050664d61f1140fc82cd
gokhan-turedi <turedi.gokhan@gmail.com>
2013-11-27 14:35:38 +0200

    commit testi icin gereksiz using ler kaldırıldı

--- Source/BookStore.Business/AuthorService.cs
+++ Source/BookStore.Business/AuthorService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using BookStore.Business.Util;
+﻿using BookStore.Business.Util;
 using BookStore.Data;
 using BookStore.Domain.Entities;
 
